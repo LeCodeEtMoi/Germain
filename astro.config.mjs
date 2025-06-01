1231da09303a0b307b7679b7fa94b2f498fd3f83
LeCodeEtMoi <lapetitelignedecode@tutanota.com>
2025-06-01 19:17:28 +0200

    Mise en place gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
     site: 'https://lecodeetmoi.github.io/Germain/',
-    base: "/Germain/",
+    base: '/Germain/',
+    integrations: [react()],
   });
