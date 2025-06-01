5c7f648568c27b09aa5b0715199dd431b63fe34c
LeCodeEtMoi <lapetitelignedecode@tutanota.com>
2025-06-01 18:31:14 +0200

    Mise en place du workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lecodeetmoi.github.io',
+  site: 'https://lecodeetmoi.github.io/Germain/',
   base: '/Germain/',
+  outDir: './dist',
 });
